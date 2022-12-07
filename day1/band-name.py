45a1a2bd5c38c93cb87f1968fa1eaf66c06f4bc0
yannick <yannickparker84@gmail.com>
2022-12-07 13:31:13 -0600

    day1

---
+++ day1/band-name.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator")
+city=input("What is the name of the city you grew up in?\n")
+pet=input("What's your pet's name?\n")
+def band_name():
+    return f" Your band name could be {city} {pet}"
+band_name()
+print(band_name())
---
+++ day1/new_line.py
@@ -0,0 +1,1 @@
+print("Hello world\nJe suis un kmer\n  vivant aux USa")
